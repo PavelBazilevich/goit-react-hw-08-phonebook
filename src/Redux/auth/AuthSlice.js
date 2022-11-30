7308d0cf51615249cd4194f76ecb5b85987c9e5b
PavelBazilevich <102550003+PavelBazilevich@users.noreply.github.com>
2022-11-30 22:58:44 +0200

    test

--- src/Redux/auth/AuthSlice.js
+++ src/Redux/auth/AuthSlice.js
@@ -11,13 +11,15 @@ const initialState = {
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  extraReducers: {
-    [register.fulfilled](state, action) {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-      state.isLoggedIn = true;
-    },
-  },
+  extraReducers: builder =>
+    builder
+      .addCase(register.pending, (state, action) => state)
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(register.rejected, (state, action) => state),
 });
 
 export const authReducer = authSlice.reducer;
